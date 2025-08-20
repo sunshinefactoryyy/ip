6c9fd3f03236e00e83936c46397fcac570d59f99
sunshinefactoryyy <yyuewei.wu@gmail.com>
2025-08-20 14:01:30 +0800

    Add Level-1

--- src/main/java/BobBot.java
+++ src/main/java/BobBot.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class BobBot {
     public static void main(String[] args) {
         String BOT_NAME = "BobBot";
+        String BANNER =
+            " ██████   ██████   ██████   ██████   ██████   █████████\n" +
+            "██    ██ ██    ██ ██    ██ ██    ██ ██    ██     ██    \n" +
+            "██    ██ ██    ██ ██    ██ ██    ██ ██    ██     ██    \n" +
+            "███████  ██    ██ ███████  ███████  ██    ██     ██    \n" +
+            "██    ██ ██    ██ ██    ██ ██    ██ ██    ██     ██    \n" +
+            "██    ██ ██    ██ ██    ██ ██    ██ ██    ██     ██    \n" +
+            " ███████   ██████  ██████   ██████   ██████      ██    \n" +
+            "\n" +
+            "        [  (•_•)   B O B B O T   (•_•)  ]\n" +
+            "             Your friendly chatbot\n";
+        System.out.println(BANNER);
         System.out.println("Hello! I'm " + BOT_NAME);
-        System.out.println("What can I do for you?");
-        System.out.println("__________________________________________________");
+        System.out.println("What can I do for you bobz?");
 
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("__________________________________________________");
+        boolean start = true;
+        while (start) {
+            Scanner userInput = new Scanner(System.in);
+            String inputString = userInput.nextLine();
+            if (inputString.equals("bye")) {
+                start = false;
+                System.out.println("Bye bobz. Hope to see you again soon bobz!");
+            } else {System.out.println(inputString);}
+        }
     }
 }
